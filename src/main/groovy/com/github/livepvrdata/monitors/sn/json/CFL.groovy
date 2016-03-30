51da99ed2971a9473e2938f7f60580d660887408
Derek Battams <derek@battams.ca>
2016-03-29 22:36:47 -0400

    Port caching for Mosport monitor; port caching for CFL monitor
    Ported the caching logic for these monitors and am dumping them into the
    repo, but they aren't included in the factory.  If someone else wants
    these turned back on, then they can do it.  I did not test these after
    porting them.

--- src/main/groovy/com/github/livepvrdata/monitors/sn/json/CFL.groovy
+++ src/main/groovy/com/github/livepvrdata/monitors/sn/json/CFL.groovy
@@ -13,7 +13,7 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
 */
-package com.github.livepvrdata.monitors.espn.json
+package com.github.livepvrdata.monitors.sn.json
 
 import groovy.json.JsonSlurper
 import groovy.util.logging.Log4j
@@ -25,38 +25,39 @@ import com.github.livepvrdata.common.data.resp.StatusResponse
 import com.github.livepvrdata.monitors.EventMonitor
 
 @Log4j
-abstract class MotorSportMonitor extends EventMonitor {
-	static final String FEED_URL = 'http://site.api.espn.com/apis/v2/scoreboard/header?sport=%s&league=%s'
-	static private final String DATE_FMT = 'yyyy-MM-dd\'T\'HH:mm:ssX'
-	static private final String F1_DATE_FMT = 'yyyy-MM-dd\'T\'HH:mmX'
+class CFL extends EventMonitor {
+	static final String FEED_URL = 'http://www.sportsnet.ca/wp-content/themes/sportsnet/zones/ajax-scoreboard.php'
+	static private final String DATE_FMT = 'EEE MMM dd hh:mm aa zzz yyyy'
 	static private final TimeZone FEED_TZ = TimeZone.getTimeZone('America/New_York')
 	
 	static final Event[] parseFeed(String data) throws IOException {
-		def json = new JsonSlurper().parseText(data)
-		json.sports[0].leagues[0].events.collect {
-			def startTime
-			try {
-				startTime = Date.parse(DATE_FMT, it.date)
-			} catch(ParseException) {
-				startTime = Date.parse(F1_DATE_FMT, it.date)
+		def events = []
+		def year = new Date().format('yyyy', FEED_TZ)
+		def json = new JsonSlurper().parseText(data).data.cfl
+		def states = json.keySet().findAll { json[it] instanceof Collection }
+		states.each { state ->
+			json."$state".each {
+				def startTime = Date.parse(DATE_FMT, "$it.date $it.time $year")
+				def status
+				if(state == 'Pre-Game')
+					status = "${startTime.format('MMM d H:mm', FEED_TZ)} ET"
+				else if(it.clock)
+					status = "$it.clock ($it.period)"
+				else
+					status = it.game_status
+				def teams = [it.visiting_team_city, it.home_team_city] as String[]
+				events << new Event(teams, status.toLowerCase().matches('.*(?:final|postponed|cancell?ed).*'), status, startTime.time)
 			}
-
-			def status
-			if(it.status == 'pre')
-				status = "${startTime.format('MMM d H:mm', FEED_TZ)} ET"
-			else
-				status = it.status == 'post' ? 'Race Complete' : 'In Progress'
-			def teams = [it.name]
-			new Event(teams as String[], it.status == 'post', status, startTime.time)
 		}
+		events
 	}
 
-	static final Event[] getFeed(String sport, String league) throws IOException {
-		String key = String.format("espnjson_%s_%s", sport, league)
+	static final Event[] getFeed() throws IOException {
+		String key = 'sn_cfl'
 		def events = AppRuntime.instance.statusCache.get(key)
 		if(events == null) {
 			try {
-				def data = new URL(String.format(FEED_URL, sport, league)).text
+				def data = new URL(FEED_URL).text
 				def parsedData = parseFeed(data)
 				def element = new Element(key, parsedData)
 				element.timeToLive = 120
@@ -70,43 +71,37 @@ abstract class MotorSportMonitor extends EventMonitor {
 			events = events.objectValue
 	}
 
-	static public Event[] getEventsForDate(String sport, String league, long date) throws IOException {
+	static public Event[] getEventsForDate(long date) throws IOException {
 		def matchFmt = 'yyyyMMdd'
-		getFeed(sport, league).findAll {
+		getFeed().findAll {
 			new Date(date).format(matchFmt, FEED_TZ) == new Date(it.startDate).format(matchFmt, FEED_TZ)
 		}
 	}
 	
+	static final String getNormalizedTitle() { 'CFL Football' }
+	
 	private String desc
 	private String[] teams
-	protected long date
-	private String sport
-	protected String league
+	private long date
 	
-	MotorSportMonitor(String desc, long date, String sport, String league) {
+	CFL(String desc, long date) {
 		this.desc = desc
-		this.sport = sport
-		this.league = league
 		this.date = date
 	}
 	
-	MotorSportMonitor(String[] teams, long date, String sport, String league) {
+	CFL(String[] teams, long date) {
 		this.teams = teams
-		this.desc = teams[0]
-		this.sport = sport
-		this.league = league
 		this.date = date
 	}
 
 	@Override
 	public StatusResponse execute() throws IOException {
-		def events = getEventsForDate(sport, league, date) 
-		def e = events.size() == 1 ? events[0] : events.find { Math.abs(date - it.startTime.time) <= 3600000L }
+		def searchTerms = !teams ? EventMonitor.generateSearchTerms(desc) : EventMonitor.findAlternatives(teams)
+		def events = getEventsForDate(date) 
+		def e = events.size() == 1 ? events[0] : events.find { it.isParticipating(searchTerms as String[]) }
 		if(e)
-			new StatusResponse(title, desc, e.status, true, !e.isComplete(), e.isComplete())
+			new StatusResponse(getNormalizedTitle(), desc, e.status, true, !e.isComplete(), e.isComplete())
 		else
 			throw new IOException("Unable to find event data! [$desc]")
 	}
-	
-	abstract String getTitle()
 }
