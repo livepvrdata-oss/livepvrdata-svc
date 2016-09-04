3a7b03626a101950698b31eb2382ac3e9d559495
skiingwiz <skiingwiz@gmail.com>
2016-09-04 00:19:08 -0400

    Monitor all of college football, rather than just Top 25

--- src/main/groovy/com/github/livepvrdata/monitors/espn/json/CFB.groovy
+++ src/main/groovy/com/github/livepvrdata/monitors/espn/json/CFB.groovy
@@ -1,12 +1,12 @@
 /*
  Copyright 2016 Battams, Derek
- 
+
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
 	You may obtain a copy of the License at
- 
+
 		http://www.apache.org/licenses/LICENSE-2.0
- 
+
 	Unless required by applicable law or agreed to in writing, software
 	distributed under the License is distributed on an "AS IS" BASIS,
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,42 @@
 */
 package com.github.livepvrdata.monitors.espn.json
 
+import com.github.livepvrdata.AppRuntime
 import com.github.livepvrdata.common.data.Event
 
+import groovy.json.JsonSlurper
+import net.sf.ehcache.Element
+
 class CFB extends Monitor {
-	static private final String SPORT = 'football'
-	static private final String LEAGUE = 'college-football'
-	static String getNormalizedTitle() { 'College Football' }
-	static Event[] getEventsForDate(long date) throws IOException {	Monitor.getEventsForDate(SPORT, LEAGUE, date) }
-	
+    private static final String SPORT = 'football'
+    private static final String LEAGUE = 'college-football'
+    private static final String CONFERENCES_URL = "http://site.api.espn.com/apis/site/v2/sports/$SPORT/$LEAGUE/scoreboard/conferences";
+
+    static String getNormalizedTitle() { 'College Football' }
+    static Event[] getEventsForDate(long date) throws IOException {	Monitor.getEventsForDate(SPORT, LEAGUE, date, fetchGroupIds()) }
+
+    public static String fetchGroupIds() {
+        String key = String.format("espnjson_%s_%s_groups", SPORT, LEAGUE)
+        def groups = AppRuntime.instance.statusCache.get(key)
+
+        if(groups == null) {
+            String data = new URL(CONFERENCES_URL).text
+            def json = new JsonSlurper().parseText(data)
+
+            String groupData = json.conferences.collect {
+                it.groupId
+            }.join(",")
+
+            def element = new Element(key, groupData)
+            element.timeToLive = 3600 * 24 * 7 //one week
+            AppRuntime.instance.statusCache.put(element)
+
+            return groupData
+        } else {
+            return groups.objectValue
+        }
+    }
+
 	CFB(String desc, long date) {
 		super(desc, date, SPORT, LEAGUE)
 	}
@@ -33,4 +61,9 @@ class CFB extends Monitor {
 
 	@Override
 	String getTitle() { getNormalizedTitle() }
+
+    @Override
+    public String getGroupIds() {
+        fetchGroupIds()
+    }
 }
--- src/main/groovy/com/github/livepvrdata/monitors/espn/json/Monitor.groovy
+++ src/main/groovy/com/github/livepvrdata/monitors/espn/json/Monitor.groovy
@@ -1,12 +1,12 @@
 /*
  Copyright 2016 Battams, Derek
- 
+
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
 	You may obtain a copy of the License at
- 
+
 		http://www.apache.org/licenses/LICENSE-2.0
- 
+
 	Unless required by applicable law or agreed to in writing, software
 	distributed under the License is distributed on an "AS IS" BASIS,
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,7 +29,7 @@ abstract class Monitor extends EventMonitor {
 	static final String FEED_URL = 'http://site.api.espn.com/apis/v2/scoreboard/header?sport=%s&league=%s'
 	static private final String DATE_FMT = 'yyyy-MM-dd\'T\'HH:mm:ssX'
 	static private final TimeZone FEED_TZ = TimeZone.getTimeZone('America/New_York')
-	
+
 	static final Event[] parseFeed(String data) throws IOException {
 		def json = new JsonSlurper().parseText(data)
 		json.sports[0].leagues[0].events.collect {
@@ -38,19 +38,24 @@ abstract class Monitor extends EventMonitor {
 			if(it.status == 'pre')
 				status = "${startTime.format('MMM d H:mm', FEED_TZ)} ET"
 			else
-				status = it.summary			
+				status = it.summary
 			def teams = it.competitors.collect { it.name } as String[]
 			def lowerStatus = status.toLowerCase()
 			new Event(teams, lowerStatus == 'ft' || lowerStatus.matches('.*(?:final|postponed|cancell?ed|full time|suspended).*'), status, startTime.time)
-		}
+		}.unique()
 	}
 
-	static final Event[] getFeed(String sport, String league) throws IOException {
+	static final Event[] getFeed(String sport, String league, String groups) throws IOException {
 		String key = String.format("espnjson_%s_%s", sport, league)
 		def events = AppRuntime.instance.statusCache.get(key)
 		if(events == null) {
 			try {
-				def data = new URL(String.format(FEED_URL, sport, league)).text
+                String url = String.format(FEED_URL, sport, league)
+                if(groups) {
+                    url += "&limit=300&groups=$groups"
+                }
+
+				def data = new URL(url).text
 				def parsedData = parseFeed(data)
 				def element = new Element(key, parsedData)
 				element.timeToLive = 120
@@ -65,26 +70,30 @@ abstract class Monitor extends EventMonitor {
 		events
 	}
 
-	static public Event[] getEventsForDate(String sport, String league, long date) throws IOException {
+    public static Event[] getEventsForDate(String sport, String league, long date) throws IOException {
+        getEventsForDate(sport, league, date, null)
+    }
+
+    static public Event[] getEventsForDate(String sport, String league, long date, String groupIds) throws IOException {
 		def matchFmt = 'yyyyMMdd'
-		getFeed(sport, league).findAll {
+		getFeed(sport, league, groupIds).findAll {
 			new Date(date).format(matchFmt, FEED_TZ) == new Date(it.startDate).format(matchFmt, FEED_TZ)
 		}
 	}
-	
+
 	private String desc
 	private String[] teams
 	private long date
 	private String sport
 	protected String league
-	
+
 	Monitor(String desc, long date, String sport, String league) {
 		this.desc = desc
 		this.sport = sport
 		this.league = league
 		this.date = date
 	}
-	
+
 	Monitor(String[] teams, long date, String sport, String league) {
 		this.teams = teams
 		this.sport = sport
@@ -95,13 +104,15 @@ abstract class Monitor extends EventMonitor {
 	@Override
 	public StatusResponse execute() throws IOException {
 		def searchTerms = !teams ? EventMonitor.generateSearchTerms(desc) : EventMonitor.findAlternatives(teams)
-		def events = getEventsForDate(sport, league, date) 
+		def events = getEventsForDate(sport, league, date, getGroupIds())
 		def e = events.size() == 1 ? events[0] : events.find { it.isParticipating(searchTerms as String[]) }
 		if(e)
 			new StatusResponse(title, desc, e.status, true, !e.isComplete(), e.isComplete())
 		else
 			throw new IOException("Unable to find event data! [$desc]")
 	}
-	
+
 	abstract String getTitle()
+
+    protected String getGroupIds() { null }
 }
